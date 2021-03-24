b1e182d61e20ff3cd7edafd8c7245b7cf4080f77
smylebifa <samoilovilia@mail.ru>
2021-03-24 14:59:15 +0300

    add landscape layout, checking status and adding info to log

--- java/ru/samoilov/geoquiz/MainActivity.kt
+++ java/ru/samoilov/geoquiz/MainActivity.kt
@@ -2,17 +2,21 @@ package ru.samoilov.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 
 private lateinit var trueButton: Button
 private lateinit var falseButton: Button
-private lateinit var prevButton: Button
-private lateinit var nextButton: Button
+private lateinit var prevButton: ImageButton
+private lateinit var nextButton: ImageButton
 private lateinit var questionTextView: TextView
+
+private const val TAG = "MainActivity"
 
 private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -28,6 +32,9 @@ private var currentIndex = 0
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d(TAG,"onCreate(Bundle?) called")
+
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -61,6 +68,32 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
 
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+
 
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
